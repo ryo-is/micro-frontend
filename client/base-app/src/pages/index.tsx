ddf14cfc52e2cd9ec739b5d4fa1469d6b629575d
ryo-is <ryosuke.izumi62@gmail.com>
2022-03-21 17:10:52 +0900

    feat: add graphql

--- client/base-app/src/pages/index.tsx
+++ client/base-app/src/pages/index.tsx
@@ -1,7 +1,34 @@
 import type { NextPage } from 'next';
+import { gql, useQuery } from '@apollo/client';
+import { Query } from '@graphql/generated/graphql';
 
 const Home: NextPage = () => {
-  return <div className="">Hello Next App!!!!</div>;
+  const query = gql`
+    query Get($userId: Int!) {
+      user(id: $userId) {
+        id
+        name
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery<Query>(query, {
+    variables: {
+      userId: 100,
+    },
+  });
+  console.log(data);
+
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {data?.user?.id} / {data?.user?.name}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
