ad365be11aa14e793aba746d90f11b86a4bfcbf8
ryo-is <ryosuke.izumi62@gmail.com>
2022-04-06 14:19:41 +0900

    feat: add hide data button

--- client/base-app/src/components/GraphQL.tsx
+++ client/base-app/src/components/GraphQL.tsx
@@ -10,9 +10,13 @@ export const GraphQL = () => {
   const [isShowData, setIsShowData] = useState(false);
 
   const handleShowData = async () => {
-    console.log('handleShowData');
     const res = await isConfirmed('データを表示しますか？');
     if (res) setIsShowData(true);
+  };
+
+  const handleHideData = async () => {
+    const res = await isConfirmed('データを非表示にしますか？');
+    if (res) setIsShowData(false);
   };
 
   if (error) {
@@ -28,10 +32,16 @@ export const GraphQL = () => {
           <div>UserID: {isShowData ? data?.user?.id : '---'}</div>
           <div>Name: {isShowData ? data?.user?.name : '---'}</div>
           <button
-            className="bg-blue-600 hover:bg-blue-700 text-gray-200 py-2 px-4 rounded mt-2"
+            className="bg-blue-600 hover:bg-blue-700 text-gray-200 py-2 px-4 rounded mt-2 mr-2"
             onClick={handleShowData}
           >
             データを表示する
+          </button>
+          <button
+            className="bg-red-600 hover:bg-red-700 text-gray-200 py-2 px-4 rounded mt-2"
+            onClick={handleHideData}
+          >
+            データを非表示にする
           </button>
         </div>
       )}
